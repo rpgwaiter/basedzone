4b870c9e081ba3c46eb96dbdca219013bcc88439
rpgwaiter <rpgwaiter@based.zone>
2020-10-21 12:48:14 -0500

    styling updates on add release form

--- app/Http/Livewire/PhysicalRelease/AddForm.php
+++ app/Http/Livewire/PhysicalRelease/AddForm.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\PhysicalRelease;
 
 use Livewire\Component;
 use App\Models\Media;
+use Livewire\WithFileUploads;
 use App\Models\PhysicalRelease;
 
 class AddForm extends Component
@@ -11,6 +12,8 @@ class AddForm extends Component
     public $release_type;
     public $imdb_id;
     public $mediaID; // for future reference, no underscores in vars passed in through livewire!
+    public $scans;
+    use WithFileUploads;
 
     public function render()
     {
--- resources/views/physical_release/create.blade.php
+++ resources/views/physical_release/create.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="max-w-7xl m-auto sm:px-6 lg:px-8">
+    <div class="m-auto max-w-xl">
         <livewire:welcome-banner />
         <div class="justify-center mt-8 bg-teal-500 dark:bg-gray-800 shadow-lg sm:rounded-lg">
 
